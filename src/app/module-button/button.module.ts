94b98d69a154c9377b30efe257bb712c8d4d3709
devshum <57689318+devshum@users.noreply.github.com>
2022-06-02 13:52:23 +0300

    button was defined

--- src/app/module-button/button.module.ts
+++ src/app/module-button/button.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './components/button/button.component';
+import { SvgModule } from '../module-svg/svg.module';
 
 
 
@@ -9,7 +10,8 @@ import { ButtonComponent } from './components/button/button.component';
     ButtonComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SvgModule
   ],
   exports: [
     ButtonComponent
