40fff30c390c1b7c9e477f2d16bae865f904baeb
devshum <57689318+devshum@users.noreply.github.com>
2022-06-03 12:22:22 +0300

    article components was styled and set up

--- src/app/module-article-details/article-details/article-details.component.ts
+++ src/app/module-article-details/article-details/article-details.component.ts
@@ -1,4 +1,10 @@
+import { ArticlesDashboardService } from './../../core/services/articles-dashboard.service';
+import { Observable } from 'rxjs';
+
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ActivatedRoute, Params, } from '@angular/router';
+import { map, switchMap } from 'rxjs/operators';
+import { Article } from 'src/app/core/interfaces/article.interface';
 
 @Component({
   selector: 'app-article',
@@ -7,10 +13,16 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ArticleDetailsComponent implements OnInit {
-
-  constructor() { }
+  public article$: Observable<Article>;
+  constructor(
+    private _activatedRoute: ActivatedRoute,
+    private _articlesService: ArticlesDashboardService
+  ) { }
 
   ngOnInit(): void {
+    this.article$ = this._activatedRoute.params.pipe(
+      map((params: Params) => params.id),
+      switchMap((id: string) => this._articlesService.getArticle(id))
+    );
   }
-
 }
