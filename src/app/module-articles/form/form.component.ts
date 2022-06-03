837f6b7eab8e5c9d9d7defadb2db1226816221ff
devshum <57689318+devshum@users.noreply.github.com>
2022-06-03 08:24:39 +0300

    reactive approach was set

--- src/app/module-articles/form/form.component.ts
+++ src/app/module-articles/form/form.component.ts
@@ -1,5 +1,6 @@
-import { NgForm } from '@angular/forms';
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Component, ChangeDetectionStrategy, OnInit, Output, EventEmitter } from '@angular/core';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-form',
@@ -7,13 +8,14 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./form.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FormComponent {
+export class FormComponent implements OnInit {
+  @Output() search: EventEmitter<string> = new EventEmitter<string>();
+  public searchInput = new FormControl();
 
-  constructor(
-
-  ) { }
-
-  public submit(form: NgForm) {
-    console.log(form.value.search);
+  ngOnInit(): void {
+    this.searchInput.valueChanges.pipe(
+      debounceTime(500),
+      distinctUntilChanged()
+    ).subscribe(value => this.search.emit(value));
   }
 }
